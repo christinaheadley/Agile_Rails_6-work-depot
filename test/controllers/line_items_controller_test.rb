bda9a4c46a51cc1b0bfb1a2da9c2a99724386846
christina <chrstnaheadley@gmail.com>
2021-09-04 14:48:04 -0400

    adds Cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,10 +17,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      # post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } } updating bc added add to cart button and now directing to cart (where cart ID is internal states data residing in a cookie) instead of line items page
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
+    follow_redirect!
+
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', 'Programming Ruby 1.9'
+    # assert_redirected_to line_item_url(LineItem.last)
   end
 
   test "should show line_item" do
