5eee7cc64fdec49158346fefc2aa52657249e199
christina <chrstnaheadley@gmail.com>
2021-09-10 15:15:54 -0400

    adds ajax

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -26,6 +26,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2', 'Your Cart'
     assert_select 'td', 'Programming Ruby 1.9'
   end
+
+  test "should create line_item" via ajax do
+    assert_difference('LineItem.count') do
+      # post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } } updating bc added add to cart button and now directing to cart (where cart ID is internal states data residing in a cookie) instead of line items page
+      post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
+    end
+
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body
+  end
   
   test "should show line_item" do
     get line_item_url(@line_item)
